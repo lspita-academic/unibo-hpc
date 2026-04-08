bd92f75eeb0660f31ef9df8fb03f28f31b94c9c0
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2026-04-08 19:07:51 +0200

    updated readme and makefile

--- assignment/scripts/scaling.sh
+++ assignment/scripts/scaling.sh
@@ -1,36 +1,34 @@
 #!/usr/bin/env bash
 
-WORKDIR=$(echo "$(git rev-parse --show-toplevel 2>/dev/null)/assignment" || pwd)
+required_vars=( \
+    VARIANT \
+    SCALING_TYPE \
+    BASE_POINTS \
+    INPUT_CLUSTERS \
+    INPUTGEN_DIMS \
+    INPUTGEN_CLUSTERS \
+    SCALING_DIR \
+    MAKE \
+    NREPS \
+    MAX_UNITS \
+)
 
-if [ $# -le 0 ]; then
-    echo "Invalid number of arguments: $#" 1>&2
-    echo "Usage: $0 <base points> <scaling type> <variant>"
-fi
+for var in "${required_vars[@]}"; do
+  if [[ -z "${!var}" ]]; then
+    echo "Error: $var is not set or empty." 1>&2
+    exit 1
+  fi
+done
 
-INPUT_BASE_POINTS=$1 # base problem size
-SCALING_TYPE=$2 # scaling type, either "weak" or "strong"
-VARIANT=$3 # binary type, either "serial", "omp" or "mpi". Serial is just to get the table of values for comparison.
-
-if [ $INPUT_BASE_POINTS -le 0 ]; then
-    echo "Invalid base problem size: $INPUT_BASE_POINTS" 1>&2
+if [ $BASE_POINTS -le 0 ]; then
+    echo "Invalid base problem size: $BASE_POINTS" 1>&2
     exit 1
 fi
 
 if [[ "$VARIANT" == "serial" ]]; then
     MAX_UNITS=1
-else
-    MAX_UNITS=${MAX_UNITS:-$(nproc 2>/dev/null || sysctl -n hw.logicalcpu 2>/dev/null || echo 1)}
 fi
 
-INPUT_CLUSTERS=${INPUT_CLUSTERS:-5}
-NREPS=${NREPS:-5}
-BIN_DIR=${BIN_DIR:-$WORKDIR/bin}
-SCALING_DIR=${SCALING_DIR:-$WORKDIR/scaling}
-INPUTGEN_BIN=${INPUTGEN_BIN:-$BIN_DIR/inputgen}
-INPUTGEN_DIMS=${INPUTGEN_DIMS:-20}
-INPUTGEN_CLUSTERS=${INPUTGEN_CLUSTERS:-50}
-
-make build-${VARIANT}
 mkdir -p $SCALING_DIR
 
 table_header="p\tn"
@@ -41,11 +39,11 @@ for p in $(seq 1 $MAX_UNITS); do
     ENV_VARS=()
     case ${SCALING_TYPE} in
         weak)
-            INPUT_POINTS=$(( INPUT_BASE_POINTS * p ))
+            INPUT_POINTS=$(( BASE_POINTS * p ))
             ENV_VARS+=(FORCE_ITERATIONS=true)
             ;;
         strong)
-            INPUT_POINTS=$INPUT_BASE_POINTS
+            INPUT_POINTS=$BASE_POINTS
             ;;
     esac
 
@@ -60,7 +58,7 @@ for p in $(seq 1 $MAX_UNITS); do
     for rep in `seq $NREPS`; do
         OUTPUT_FILE=${FILE_BASENAME}-${SCALING_TYPE}-${VARIANT}-K${INPUT_CLUSTERS}-P${p}-R${rep}.out
         EXEC_TIME="$(
-            env ${ENV_VARS[@]} make run-$VARIANT WORK_UNITS=$p CLUSTERS=$INPUT_CLUSTERS INPUT=$INPUT_FILE OUTPUT=$OUTPUT_FILE | \
+            env ${ENV_VARS[@]} $MAKE run-$VARIANT WORK_UNITS=$p CLUSTERS=$INPUT_CLUSTERS INPUT=$INPUT_FILE OUTPUT=$OUTPUT_FILE | \
             grep "Elapsed seconds: " | \
             sed 's/Elapsed seconds: //' \
         )"
